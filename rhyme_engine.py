f9fddc624e33fc541cf98365492dbb33c8f505b8
Ashish <ashishishu286@gmail.com>
2026-02-17 00:16:16 +0530

    Rhyme Engine version 2.0

--- rhyme_engine.py
+++ rhyme_engine.py
@@ -1,6 +1,6 @@
 import re
 from cmu_loader import load_cmu_dict
-phoneme_list = load_cmu_dict()
+cmu_dict = load_cmu_dict()
 
 def find_last_stressed_vowel(phenome):
     index = len(phenome)-1
@@ -9,10 +9,10 @@ def find_last_stressed_vowel(phenome):
             return x
     return -1
 
-def extract_rhyme_part(phoneme_list):
+def extract_rhyme_part(cmu_dict):
     rhyme_words = {}
 
-    for word, pronunciations in phoneme_list.items():
+    for word, pronunciations in cmu_dict.items():
         for phonemes in pronunciations:
             index = find_last_stressed_vowel(phonemes)
 
@@ -29,21 +29,79 @@ def extract_rhyme_part(phoneme_list):
     return rhyme_words
 
 
-rhymeDict = extract_rhyme_part(phoneme_list)
+rhymeDict = extract_rhyme_part(cmu_dict)
 
 def get_rhymes(word):
     word = re.sub(r'[^a-z]', '', word.lower())
-    pronounciations = phoneme_list.get(word)
-    if not pronounciations:
+    pronunciations = cmu_dict.get(word)
+    if not pronunciations:
         print("Sorry, this word is not present in the dictionary")
     else:
         ans = set()
-        for phenome in pronounciations:
-            index = find_last_stressed_vowel(phenome)
+        for phoneme in pronunciations:
+            index = find_last_stressed_vowel(phoneme)
             if index == -1:
                 continue
-            wordKey = " ".join(phenome[index:])
+            wordKey = " ".join(phoneme[index:])
             for x in rhymeDict.get(wordKey, []):
-                if x!=word:
+                if x != word:
                     ans.add(x)
         return sorted(ans)
+
+def rhyme_score(p1, p2):
+    if len(p1) <= len(p2):
+        shorter = p1
+        longer = p2
+    else:
+        shorter = p2
+        longer = p1
+
+    max_score = 0
+    n = len(shorter)
+    m = len(longer)
+
+    # Slide shorter across longer
+    for start in range(m - n + 1):
+        score = 0
+        for i in range(n):
+            if shorter[i] == longer[start + i]:
+                score += 1
+            else:
+                break
+        max_score = max(max_score, score)
+
+    return max_score
+
+
+def get_similar_rhymes(word):
+    word = re.sub(r'[^a-z]', '', word.lower())
+    phoneme = cmu_dict.get(word)
+    if not phoneme:
+        print("Word not present in dictionary")
+    ans = set()
+    for pronunciation in phoneme:
+        lastStressIndex = find_last_stressed_vowel(pronunciation)
+        if(lastStressIndex==-1):
+            continue
+        trimmedPhoneme = pronunciation[lastStressIndex:]
+        print("SMOKE RAW:", cmu_dict.get("smoke"))
+        print("WORD:", word)
+        print("TRIMMED:", trimmedPhoneme)
+        for cmuWord, cmuPronunciations in cmu_dict.items():
+            for cmuPronunciation in cmuPronunciations:
+                cmuLastStressIndex = find_last_stressed_vowel(cmuPronunciation)
+                if (cmuLastStressIndex == -1):
+                    continue
+                cmuTrimmedPhoneme = cmuPronunciation[cmuLastStressIndex:]
+                if cmuWord == "smoke":
+                    print("RAW:", cmuPronunciation)
+                    print("TRIMMED:", cmuTrimmedPhoneme)
+                score = rhyme_score(trimmedPhoneme, cmuTrimmedPhoneme)
+                if score>=2:
+                    if cmuWord == word:
+                        continue
+                    ans.add(cmuWord)
+
+    return sorted(ans)
+
+print(get_similar_rhymes("stoked"))
